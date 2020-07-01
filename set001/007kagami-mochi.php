c3d7e2f967350db3fc3839cb104e096b0f76dd84
リョウタ <haramatsuryota@gmail.com>
2020-07-01 18:55:03 +0900

    007kagami-mochi

--- set001/007kagami-mochi.php
+++ set001/007kagami-mochi.php
@@ -1,20 +1,29 @@
 <?php
-// maxより小さければOK
-// max<＝xならNG
+// // maxより小さければOK
+// // max<＝xならNG
+// $n = fgets(STDIN);
+// $arr = [];
+// while ($values = fgets(STDIN)){
+//     array_push($arr, (int)$values);   
+// }
+// // $arr = [10,8,9,6]のような配列
+// rsort($arr);
+// $counter = 0;
+// for ($i = 0; $i < $n; $i++) {
+//     if (!empty($arr[$i+1])) {
+//         if ($arr[$i] <= $arr[$i+1]) {
+//             continue;
+//         } 
+//     }
+//     $counter++;
+// }
+// echo $counter;
+
+// 模範解答
 $n = fgets(STDIN);
-$arr = [];
-while ($values = fgets(STDIN)){
-    array_push($arr, (int)$values);   
-}
-// $arr = [10,8,9,6]のような配列
-rsort($arr);
-$counter = 0;
+$num = [];
 for ($i = 0; $i < $n; $i++) {
-    if (!empty($arr[$i+1])) {
-        if ($arr[$i] <= $arr[$i+1]) {
-            continue;
-        } 
-    }
-    $counter++;
+    $num[] = trim(fgets(STDIN));
 }
-echo $counter;
+echo count(array_unique($num));
+// array_uniqueで重複を消す
